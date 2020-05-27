53c502cf71171c49307a48e5ab35873d7d4fffcb
thebinary <binary4bytes@gmail.com>
2020-05-27 15:28:20 +0545

    refactor healtcheck code

--- healthcheck_server.go
+++ healthcheck_server.go
@@ -1,21 +1,22 @@
 package main
 
-//go:generate protoc -I ./protobuf --go_out=plugins=grpc:./protobuf ./protobuf/healthcheck.proto
-
 import (
 	"context"
 
 	"google.golang.org/grpc/health/grpc_health_v1"
 )
 
+// HealthCheckServer is implementation of google grpc_health_v1 interface
 type HealthCheckServer struct{}
 
+// Check implements the Check method of the interface
 func (s *HealthCheckServer) Check(ctx context.Context, request *grpc_health_v1.HealthCheckRequest) (response *grpc_health_v1.HealthCheckResponse, err error) {
 	return &grpc_health_v1.HealthCheckResponse{
 		Status: grpc_health_v1.HealthCheckResponse_SERVING,
 	}, nil
 }
 
+// Watch implements the Watch method of the interface
 func (s *HealthCheckServer) Watch(request *grpc_health_v1.HealthCheckRequest, srv grpc_health_v1.Health_WatchServer) (err error) {
 	err = srv.Send(&grpc_health_v1.HealthCheckResponse{
 		Status: grpc_health_v1.HealthCheckResponse_SERVING,
